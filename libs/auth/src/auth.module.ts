446eacd5d80a9882fa717fa35220dcba42e76769
coder <dnstylish@gmail.com>
2022-05-24 01:02:26 +0700

    init

--- libs/auth/src/auth.module.ts
+++ libs/auth/src/auth.module.ts
@@ -6,38 +6,37 @@ import { PassportModule } from '@nestjs/passport'
 import { JwtModule } from '@nestjs/jwt'
 import { JwtStrategy } from '@app/auth/passport/jwt.strategy'
 
-@Module({
-  imports: [
-    ClientsModule.registerAsync([
-      {
-        name: 'AUTH_SERVICE',
-        imports: [ConfigModule],
-        inject: [ConfigService],
-        useFactory: async (config: ConfigService) => {
-          return {
-            transport: Transport.RMQ,
-            options: {
-              urls: [config.get<string>('USER_RMQ')],
-              queue: 'users_queue',
-              queueOptions: {
-                durable: false
-              }
-            }
+export const AUTH_SERVICE = ClientsModule.registerAsync([
+  {
+    name: 'AUTH_SERVICE',
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (config: ConfigService) => {
+      return {
+        transport: Transport.RMQ,
+        options: {
+          urls: [config.get<string>('USER_RMQ')],
+          queue: 'users_queue',
+          queueOptions: {
+            durable: false
           }
         }
       }
-    ]),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        secret: config.get<string>('USER_SECRET'),
-        signOptions: { expiresIn: '3d' }
-      })
-    }),
-    ConfigModule,
-    PassportModule
-  ],
+    }
+  }
+])
+
+export const JWT_MODULE = JwtModule.registerAsync({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: async (config: ConfigService) => ({
+    secret: config.get<string>('USER_SECRET'),
+    signOptions: { expiresIn: '3d' }
+  })
+})
+
+@Module({
+  imports: [AUTH_SERVICE, JWT_MODULE, ConfigModule, PassportModule],
   providers: [AuthService, JwtStrategy],
   exports: [AuthService]
 })
